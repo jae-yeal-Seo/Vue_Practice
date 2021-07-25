ba4701821bbc9656ab8b11f0323f4bbf646ce5e0
woduf9173@naver.com <woduf9173@naver.com>
2021-07-25 18:56:54 +0900

    결과적으로 반복문이였기 때문에 부모태그가 3개였던것. 그래서 <div>로 감싸야

--- vue.js
+++ vue.js
@@ -1,27 +1,30 @@
-Vue.component('todo-item',{
-    props:['todo'],
-    template:'<li>{{todo.text}}</li>'
-})
+// Vue.component('todo-item',{
+//     props:['todo'],
+//     template:`
+//     <div>
+//     <li v-for="item in grocerylist" v-bind:todo="item" v-bind:key="item.id">{{todo.text}}</li>
+//     </div>
+//     `
+// })
 
 Vue.component('supply-item',{
     props:['supplyitem'],
-    template:'<li>{{supplyitem.text}}</li>'
+    template:'<li v-for="supply in supplylist" v-bind:supplyitem="supply" v-bind:key="supply.id">{{supply.text}}</li>',
+    data(){
+        return{
+            supplylist:[
+                {id:'1', text:'first'},
+                {id:'2', text:'seconde'},
+                {id:'3', text:'third'}
+            ],
+            forerror:'whaterror'
+        } 
+    }
+
 })
 
 let app = new Vue({
-    el:'#app-7',
-    data:{
-        grocerylist:[
-            {id:'1',text:'first sentence'},
-            {id:'2',text:'middle sentence'},
-            {id:'3',text:'final sentence'}
-        ],
-        supplylist:[
-            {id:'1', text:'first supply'},
-            {id:'2', text:'second supply'},
-            {id:'3', text:'third supply'}
-        ]
-    }
+    el:'#app-7'
 })
 
 
