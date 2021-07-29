ac1afada84d178f1abfeabc69618ae35c2f33262
woduf9173@naver.com <woduf9173@naver.com>
2021-07-29 12:46:06 +0900

    vue 수업 복습

--- vue.js
+++ vue.js
@@ -1,15 +1,13 @@
 let app = new Vue({
     el:'#app',
     data:{
-        user:{
-            userid:'user123',
-            name:'Jane Vue',
-            email:'user123@email.com'
-        },
-        users:[
-            {password:150303},
-            {password:250303},
-            {password:350303}
-        ]
+     user_id:'user123',
+     user_plus:'plus',
+     user_grade:'Silver'
+    },
+    computed:{
+        user_info(){
+            return this.user_id+'('+this.user_grade+')'
+        }
     }
 });
